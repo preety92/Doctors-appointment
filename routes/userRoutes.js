efd2103d7675229f72588c9bd892bb33d2c5d94b
preety thakur <preetythakur1992@gmail.com>
2024-02-15 13:24:59 -0500

    connecting client server,auth

---
+++ routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express=require('express')
+const { loginController,registerController, authController } = require('../controllers/userCtrl');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+//router onject
+const router=express.Router()
+
+//routes
+//login||post
+
+router.post("/login",loginController);
+
+//register||post 
+router.post("/register",registerController);
+router.post("/getUserData",authMiddleware,authController);
+module.exports=router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express=require('express')
-const colors=require('colors')
-const morgan=require('morgan')
-const dotenv=require('dotenv')
+const express=require('express');
+const colors=require('colors');
+const morgan=require('morgan');
+const dotenv=require('dotenv');
+const connectDB = require('./config/db');
+
 
 //dotenv config
 dotenv.config();
+
+//mogodb connection
+connectDB();
 
 const app=express()
 
@@ -13,17 +18,13 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 //routes
-app.get("/",(req,res)=>{
-    res.status(200).send({
-        message:"server running",
-    });
-});
+app.use('/api/v1/user',require("./routes/userRoutes"))
 
 //listen port
 const port=process.env.PORT || 8080
 app.listen(port,()=>{
     console.log(
-        `server running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+        `server running in ${process.env.DEV_MODE} Mode on port ${process.env.PORT}`
         .bgCyan.white
     );
 });
